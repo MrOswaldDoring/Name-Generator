5bdc1a533842b95ad35e53712518b7748d534b2b
Oswald <oswald.doring98@hotmail.com>
2017-06-11 21:31:09 +1000

    Added greater functionality
    Users can now add 'c' or 'v' as a letter of their choice

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ consonants = "bcdfghjklmnpqrstvwxz"
 user_name = ""
 inputs = []
 
+
 user_input_1 = input("How many letters would you like the name to be?: ")
 user_input_2 = input("How many names would you like to generate?: ")
 
@@ -12,15 +13,15 @@ name_length = int(user_input_1)
 num_of_names = int(user_input_2)
 
 for num in range(name_length):
-		inputs.append(input("Enter 'v' for vowels, 'c' for consonants, or enter your letter of choice: "))
+	inputs.append(input("Enter '1' for vowels, '2' for consonants, or enter your letter of choice: "))
 
 def text_generator():
 	global user_name
 
 	for i in inputs:
-		if i == 'v':
+		if i == '1':
 			user_name += random.choice(vowels)
-		elif i == 'c':
+		elif i == '2':
 			user_name += random.choice(consonants)
 		else:
 			user_name += i
